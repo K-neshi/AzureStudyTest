5296feeb148996535e8d22b54ec5728a7c2ee41a
K-neshi <ydgh26@icloud.com>
2024-05-20 11:59:58 +0900

    Update index.js

--- index.js
+++ index.js
@@ -21,6 +21,12 @@ const countdown = setInterval(function() {
   if(TIME <= 0) finish();
 }, 1000);
 
+function finish() {
+  clearInterval(countdown);
+  subject.textContent = '正解数は' + count + '個でした！';
+  state = false;
+}
+
 function changeColor(judg){
   if(judg === '○'){
     //文字を赤色に変更する
@@ -49,16 +55,23 @@ form.btn.addEventListener('click', function(e) {
   }
 });
 
-function init() {
+function createText() {
+  //ランダムに問題を出題
   const rnd = Math.floor(Math.random() * textList.length);
 
-  subject.textContent = textList[rnd];
-  form.input.value = '';
-  form.input.focus();
-}
+  //前の文字列を削除してから次の文字列を表示する
+  subject.textContent = '';
 
-function finish() {
-  clearInterval(countdown);
-  subject.textContent = '正解数は' + count + '個でした！';
-  state = false;
+  //文字列を1文字ずつに分解して、それぞれにspanタグを挿入する
+  checkTexts = textList[rnd].split('').map(function(value) {
+    var span = document.createElement('span');
+    span.textContent = value;
+    subject.appendChild(span);
+
+    return span;
+  });
+
+ /* subject.textContent = textList[rnd];
+  form.input.value = '';
+  form.input.focus();*/
 }
